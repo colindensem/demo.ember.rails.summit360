ac4e7685846e8b452a6fa46b64b996b63f71cf9e
Colin Densem <colindensem@summit360.co.uk>
2014-10-27 22:32:55 +0000

    Redis tweak

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,3 +67,4 @@ namespace :deploy do
   end
 
 end
+
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,4 +4,4 @@ uri = if Rails.env.development?
         URI.parse ENV['REDIS_URL']
       end
 
-$redis = Redis.new Redis.new(:url => ENV['REDIS_URL'])
+$redis = Redis.new Redis.new(:url => uri)
